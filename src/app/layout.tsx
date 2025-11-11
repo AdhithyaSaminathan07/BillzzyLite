199ae593b01736e5d3bc8fb46cb213a72b4c99bc
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2025-11-11 14:39:42 +0530

    updated the pwa error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,21 +68,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <NextAuthSessionProvider>
           {children}
         </NextAuthSessionProvider>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                    console.log('SW registered: ', registration);
-                  }).catch(function(registrationError) {
-                    console.error('SW registration failed: ', registrationError);
-                  });
-                });
-              }
-            `,
-          }}
-        />
       </body>
     </html>
   );
