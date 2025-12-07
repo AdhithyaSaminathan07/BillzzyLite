18cb11eda842bb3e462a9031ed2bfe9704da746d
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2025-12-07 15:24:10 +0530

    Remove landing page and restore login as entry point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,8 @@
+import { LoginForm } from '@/components/forms/loginform';
 
-// //src/app/page.tsx
-// import { LoginForm } from '@/components/forms/loginform';
-
-// // This is the default export that Next.js will render for the root route ('/')
-// export default function LoginPage() {
-//   return (
-//         <LoginForm />
-//   );
-// }
-
-
-
-
-import HeroSection from "@/components/landing-sections/HeroSection";
-import IntroSection from "@/components/landing-sections/IntroSection";
-import ComparisonSection from "@/components/landing-sections/ComparisonSection";
-import WhyBillzzySection from "@/components/landing-sections/WhyBillzzySection";
-import GoPaperlessSection from "@/components/landing-sections/GoPaperlessSection";
-import UseCasesSection from "@/components/landing-sections/UseCasesSection";
-import PricingSection from "@/components/landing-sections/PricingSection";
-import FAQSection from "@/components/landing-sections/FAQSection";
-import TestimonialsSection from "@/components/landing-sections/TestimonialsSection";
-// import FeaturesSection from "../../components/landing-sections/FeaturesSection";
-import CTASection from "@/components/landing-sections/CTASection";
-import Footer from "@/components/landing-sections/Footer";
-
-export default function LandingPage() {
+// This is the default export that Next.js will render for the root route ('/')
+export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-white">
-      <HeroSection />
-      <IntroSection />
-      <ComparisonSection />
-      <WhyBillzzySection />
-      <GoPaperlessSection />
-      <UseCasesSection />
-      <PricingSection />
-      <FAQSection />
-      <TestimonialsSection />
-      {/* <FeaturesSection /> */}
-      <CTASection />
-      <Footer />
-    </div>
+    <LoginForm />
   );
 }
